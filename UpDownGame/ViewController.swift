4fc33157f5a902463342527b76a4db26e73713cc
minggging <109min1004@naver.com>
2022-10-17 23:36:02 +0900

    첫번째 커밋 -  각 버튼 기능 완료(info 제외), 슬라이더 정수 변환, 5회 카운트, 슬라이더 asset, 아이콘 asset 추후 알림창 만들기

--- UpDownGame/ViewController.swift
+++ UpDownGame/ViewController.swift
@@ -9,11 +9,69 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var randomValue: Int = 0
+    var tryCount: Int = 0
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var tryCountLabel: UILabel!
+    @IBOutlet weak var sliderValueLabel: UILabel!
+    @IBOutlet weak var minimumValueLabel: UILabel!
+    @IBOutlet weak var maximumValueLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        slider.setThumbImage(#imageLiteral(resourceName: "slider_thumb.png"), for: .normal)
+        reset()
+        
     }
 
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        print(sender.value)
+        let integerValue: Int = Int(sender.value)
+        sliderValueLabel.text = String(integerValue)
+    }
 
+    @IBAction func touchUpHitButton(_ sender: UIButton) {
+        print(slider.value)
+        let hitValue: Int = Int(slider.value)
+        slider.value = Float(hitValue)
+        
+        tryCount = tryCount + 1
+        tryCountLabel.text = "\(tryCount) / 5"
+        
+        if randomValue == hitValue {
+            print("YOU HIT!!")
+            reset()
+            return
+        }
+        
+        if tryCount >= 5 {
+            print("YOU LOSE...")
+            reset()
+            return
+        }
+        
+    }
+    
+    @IBAction func touchUpResetButton(_ sender:UIButton) {
+        print("touch up reset button")
+        
+        reset()
+    }
+    
+    func reset() {
+        print("reset!")
+        randomValue = Int.random(in: 0...30)
+        print(randomValue)
+        tryCount = 0
+        tryCountLabel.text = "0 / 5"
+        slider.minimumValue = 0
+        slider.maximumValue = 30
+        slider.value = 15
+        minimumValueLabel.text = "0"
+        maximumValueLabel.text = "30"
+        sliderValueLabel.text = "15"
+    }
 }
 
